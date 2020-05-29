dda6c35e3fe61c21cab0d22fea12256bd7716550
unknown <rafacs1404@gmail.com>
2020-05-29 09:34:12 -0300

    Tudo Completo

---
+++ Aula06Heranca/CNPJ.cs
@@ -0,0 +1,14 @@
+namespace Aula06Heranca
+{
+    public class CNPJ : Pessoa
+    {
+        public string cnpj;
+
+        public bool ValidarCNPJ(){
+            if(cnpj != ""){
+                return true;
+            }
+            return false;
+        }   
+    }
+}
---
+++ Aula06Heranca/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula06Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPF individuo = new CPF();
+            individuo.nome = "Rafael";
+            Console.WriteLine(individuo.Saudar());
+            individuo.cpf = "42113562839";
+            individuo.rg = "3.021.415";
+            Console.WriteLine(individuo.ValidarCPF());
+
+        }
+    }
+}
